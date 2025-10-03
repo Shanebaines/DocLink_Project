c4e1ee321daba563a68584074305039da24cc0cd
Dewnith23 <e21005@eng.pdn.ac.lk>
2025-10-03 07:11:18 +0530

    clear codes

--- src/main/java/com/springbootpractice/doclink/Dealer/DoctorRepository.java
+++ src/main/java/com/springbootpractice/doclink/Dealer/DoctorRepository.java
@@ -2,8 +2,6 @@ package com.springbootpractice.doclink.Dealer;
 
 
 import com.springbootpractice.doclink.Kernal.Entity.Doctor;
-import com.springbootpractice.doclink.Listner.Dto.Response.ViewDoctorsDto;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
