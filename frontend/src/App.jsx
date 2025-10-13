542b2fe2494a29251e9d66076349977f92ff070b
Deeshan <e21444@eng.pdn.ac.lk>
2025-10-14 00:22:53 +0530

    Frontend pages routing structure added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DoctorLocationMap from "./pages/DoctorLocationMap";
 
 export default function App() {
   return (
-    <>
-      <DoctorLocationMap />
-    </>
+    <BrowserRouter>
+      <Routes>
+        // Add your page routes here...
+        
+        <Route path="/" element={<DoctorLocationMap />} />
+
+
+      </Routes>
+    </BrowserRouter>
   );
 }
