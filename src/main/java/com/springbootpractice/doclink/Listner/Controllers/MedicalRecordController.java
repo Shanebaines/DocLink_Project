2c60b1a80deb8d78bfc575ff62be30f89668c911
Dewnith23 <e21005@eng.pdn.ac.lk>
2025-11-24 17:35:14 +0530

    version

--- src/main/java/com/springbootpractice/doclink/Listner/Controllers/MedicalRecordController.java
+++ src/main/java/com/springbootpractice/doclink/Listner/Controllers/MedicalRecordController.java
@@ -1,6 +1,7 @@
 package com.springbootpractice.doclink.Listner.Controllers;
 
-import com.springbootpractice.doclink.Listner.Dto.Request.CreateMedicalRecordDto;
+import com.springbootpractice.doclink.Kernal.Service.MedicalRecordService;
+import com.springbootpractice.doclink.Listner.Dto.Request.CreateAppointmentRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicalRecord")
 @AllArgsConstructor
 public class MedicalRecordController {
-    ResponseEntity<?> addMedicalRecord(CreateMedicalRecordDto createMedicalRecordDto) {
-        return null;
+    public final MedicalRecordService medicalRecordService;
+
+    @PostMapping("/addRecord")
+    ResponseEntity<?> addMedicalRecord(CreateAppointmentRequestDto createAppointmentRequestDto) {
+        return medicalRecordService.addMedicalRecord(createAppointmentRequestDto);
     }
 }
